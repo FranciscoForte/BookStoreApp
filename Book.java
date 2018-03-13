4a8e13c0a93b5655155d4dbd37c258539f0aa234
FranciscoForte <36989050+FranciscoForte@users.noreply.github.com>
2018-03-13 12:19:26 +0000

    Add files via upload
    Created Book Class and index view 
    
    Created a Models Book with 4 parameters (id, name, price and author) and a "add", "remove", "findById" and "allBooks" methods. Implemented the index method on BooksController, which calls the view.html.books.index, which then shows all the books

---
+++ Book.java
@@ -0,0 +1,54 @@
+package models;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class Book {
+
+    public Integer id;
+    public String title;
+    public Integer price;
+    public String author;
+
+    public Book(Integer id, String title, Integer price, String author){
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.author = author;
+    }
+
+    private static Set<Book> books;
+
+    static {
+        books = new HashSet<>();
+        books.add(new Book(1,"C++",20,"ABC"));
+        books.add(new Book(2,"Java",30,"XYZ"));
+    }
+
+    public static Set<Book> allBooks(){
+        return books;
+    }
+
+    public static Book findById(Integer id){
+        for (Book book : books){
+            if(id.equals(book.id)){
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public static void add(Book book){
+        books.add(book);
+    }
+
+    public static boolean remove(Book book){
+        return books.remove(book);
+    }
+
+
+
+
+
+
+}
